8fda87c49e0ed1263209097bf1bf6e7d4350bd50
Dallas DeBruin <dallas.debruin.ctr>
2024-03-07 18:33:49 -0500

    Making Progress

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char** argv)
 {
     std::vector<double> arguments({0.1,0.2,0.3});
-    mathex::mathex("",arguments);
+    mathex::mathex("A+B+C",arguments);
     //std::cout << "Hello World" << std::endl;
     return 0;
 }
--- mathex.h
+++ mathex.h
@@ -9,51 +9,142 @@
 //                 / /   | |_  |  '.'| |     | |    `''-...... -'     .'  .'`.   `.
 //                 \ \._,\ '/  |   / | '.    | '.                   .'   /    `.   `.
 //                  `--'  `"   `'-'  '---'   '---'                 '----'       '----'
+#include <math.h>
+#include <stdexcept>
 #include "peglib.h"
 
 #pragma once
 
 namespace mathex {
 
-// 1. Count number of variables given to the function
-// 2. Figure out what letter combination that count == (i.e. 27 = AA)
-// 3. Walk the string, looking for capital letter groupings AAA
-//     a. Note the letters found in an ordered Set
-//          1. If a group is already present, error
-//          2. If Set.size() > number expected, error
-//     b. If find A and find count letter (e.g. AFE), note that
-//     c. Replace the captured string with the right variable entry
-// 4. Once done walking, if Count != Number Found or Not A and or !AFE, then error
-// 5. Now we have a single string to parse with Peg... do so, returning errors as needed
-
-std::string getLetters(uint64_t num)
-{
-    num--;
-    if(num == 0) {
-        return "A";
-    }
-    std::string retString;
-    while(num > 0) {
-        retString += static_cast<char>((num % 26)+65);
-        num = num / 26;
-    }
-    return retString;
-}
+// 1. Parse with PEG Grammar
+// 2. Recurse through AST
+//     a. For each variable, find the corresponding argument.
+//          b. If the variable is larger than max num arguments, error
+//          c. track with a count var, number replaced
+// 3. After parsing, ensure the count var is equal to number of arguments
 
 uint64_t getNumber(std::string letters)
 {
     uint64_t retVal = 0;
+    bool lowOrder = true;
     for(int i = 0; i < letters.length(); i++) {
-        retVal += static_cast<uint8_t>(letters.at(i)-65);
+        uint64_t hexValue = static_cast<uint64_t>(letters.at(i)) - 65;
+        if(lowOrder) retVal |= hexValue; else retVal |= hexValue << 4;
+        lowOrder = !lowOrder;
     }
     return retVal;
 }
 
+double parseAST(const peg::Ast &ast, std::vector<double>& args, bool& error) {
+    constexpr uint64_t EXPRESSION_ATOMIC(0);
+    constexpr uint64_t NUMBER(1);
+    constexpr uint64_t VARIABLE(2);
+    constexpr uint64_t FUNCTION(3);
+    constexpr uint64_t OPERATOR(4);
+
+    std::unordered_map<std::string, uint64_t> operationLookup = {
+        {"Expression", EXPRESSION_ATOMIC},
+        {"Atomic", EXPRESSION_ATOMIC},
+        {"Number", NUMBER},
+        {"Variable", VARIABLE},
+        {"Function", FUNCTION},
+        {"Operator", OPERATOR},
+    };
+
+    if(!operationLookup.count(ast.name)) {
+        error = true;
+        std::cerr << "AST error, had an unexpected Name" << std::endl;
+        return 0.0;
+    }
+
+    const auto &nodes = ast.nodes;
+
+    switch(operationLookup[ast.name]) {
+        case EXPRESSION_ATOMIC:
+            return parseAST(*nodes[1], args, error);
+        case NUMBER:
+            return nodes[0]->token_to_number<double>();
+            break;
+        case VARIABLE:
+            auto varNum = getNumber(nodes[0]->token_to_string());
+            //TODO
+            break;
+        case FUNCTION:
+            //TODO
+            break;
+        case OPERATOR:
+            //TODO
+            break;
+    }
+}
+
+bool evaluateMathexAST(const peg::Ast &ast, std::vector<double>& args, double epislon = std::numeric_limits<double>::epsilon()) {
+    bool error = false;
+    double result = parseAST(ast, args, error);
+    return !error && (std::abs(result) < epislon);
+}
+
 int64_t mathex(std::string exp, std::vector<double>& args)
 {
-    uint64_t count = args.size();
-    std::string lastLetters = getLetters(count);
-    std::cout << lastLetters << ":" << getNumber(lastLetters) << std::endl;
+    peg::parser parser(R"(
+        Expression  <-  Atomic (Operator Atomic)*
+        Atomic      <-  Number
+                    /   Variable
+                    /   Function '(' Expression ')'
+                    /   '(' Expression ')'
+        Number      <-  [0-9]+ '.' [0-9]+
+                    /   [0-9]+
+        Variable    <-  [A-P]+
+        Function    <-  'abs'
+                    /   'max'
+                    /   'min'
+                    /   'log'
+                    /   'pow'
+                    /   'sin'
+                    /   'cos'
+                    /   'tan'
+                    /   'asin'
+                    /   'acos'
+                    /   'atan2'
+                    /   'sinh'
+                    /   'cosh'
+                    /   'tanh'
+                    /   'asinh'
+                    /   'acosh'
+                    /   'atanh2'
+                    /   'ceil'
+                    /   'floor'
+        Operator    <-  '+'
+                    /   '-'
+                    /   '*'
+                    /   '/'
+                    /   '%'
+                    /   '<'
+                    /   '>'
+                    /   '<='
+                    /   '>='
+                    /   '=='
+                    /   '||'
+                    /   '&&'
+                    /   '&'
+                    /   '|'
+                    /   '^'
+                    /   '~'
+                    /   '!'
+    )",peg::Rules());
+    parser.enable_ast();
+    parser.enable_packrat_parsing();
+
+    std::shared_ptr<peg::Ast> ast;
+    if (parser.parse(exp, ast)) {
+        //ast = parser.optimize_ast(ast);
+        std::cout << ast_to_s(ast);
+        //std::cout << exp << " = " << eval(*ast) << std::endl;
+        return 0;
+    } else {
+        //error
+    }
     return true;
 }
 
